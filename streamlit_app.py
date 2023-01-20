42894818755fa1b4b8c016d42f510cd626cb836e
SunilTetali <123129017+SunilTetali@users.noreply.github.com>
2023-01-19 18:30:16 -0600

    Update streamlit_app.py
    Added some more items to the menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.title('Breakfast Menu')
+streamlit.title('Sandwich')
+Streamlit.title('Boiled Eggs')
+Streamlit.title('Oats')
