05e34ab568592eea85d85afc71535f26d22dae2c
SunilTetali <123129017+SunilTetali@users.noreply.github.com>
2023-01-19 18:31:14 -0600

    Update streamlit_app.py
    Code correction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.title('Breakfast Menu')
 streamlit.title('Sandwich')
-Streamlit.title('Boiled Eggs')
-Streamlit.title('Oats')
+streamlit.title('Boiled Eggs')
+streamlit.title('Oats')
